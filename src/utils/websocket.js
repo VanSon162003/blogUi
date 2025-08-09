94402eb78532ec3ffe354f2879686f1cc04ba9c3
van son <vanson16032003@gmailcom>
2025-08-09 23:31:06 +0700

    Fix pusher

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -2,12 +2,12 @@ import PusherJS from "pusher-js";
 
 const socketClient = new PusherJS("app1", {
     cluster: "",
-    wsHost: "103.20.97.228",
-    wsPort: 6001,
-    forceTLS: false,
+    wsHost: "chat.vsron.site",
+    wsPort: 443,
+    forceTLS: true,
     encrypted: true,
     disableStats: true,
-    enabledTransports: ["ws"],
+    enabledTransports: ["ws", "wss"],
 });
 
 export default socketClient;
