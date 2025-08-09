123d3c01174e3d7708e592d293d704d86d94e49e
van son <vanson16032003@gmailcom>
2025-08-09 21:38:07 +0700

    updates

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -2,9 +2,9 @@ import PusherJS from "pusher-js";
 
 const socketClient = new PusherJS("app1", {
     cluster: "",
-    wsHost: import.meta.env.VITE_WSHOST,
-    wsPort: import.meta.env.VITE_WSPORT,
-    forceTLS: import.meta.env.VITE_FORCETLS,
+    wsHost: "103.20.97.228",
+    wsPort: 6001,
+    forceTLS: false,
     encrypted: true,
     disableStats: true,
     enabledTransports: ["ws", "wss"],
